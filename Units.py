3349f1b473d405a15577c09fc212d8624ea5c3c4
Nathartacus <67975196+NJStriebel@users.noreply.github.com>
2020-07-08 08:43:30 -0400

    Update Units.py
    Added the IndustrialComplex class and a variable to the Unit class for storing the power that controls the unit. Updated all constructors to reflect this, initializing power to None.

--- Units.py
+++ Units.py
@@ -5,7 +5,7 @@ class Unit(object):
     Represents a unit in the game of axis and allies.
         
     """
-    def __init__(self,attack=0,defense=0,cost=0,move=0,has_moved=0):
+    def __init__(self,attack=0,defense=0,cost=0,move=0,has_moved=0,power=None):
         """
         Default parameters set attack, defense, cost, move, and has_moved to 0
         """
@@ -14,13 +14,17 @@ class Unit(object):
         self.__cost = cost
         self.__move = move #maximum number of spaces this unit can move
         self.__has_moved = has_moved #numbe of spaces moved this turn
+        self.__power = power
         
     def __str__(self):
-        return "Unit"
+        return "unit"
     
     def __repr__(self):
-        return "Unit"
+        return "unit"
         
+	def unit_type(self):
+		return "unit"
+	
     def roll_die(self):
         """
         Return a random number from 1-6 inclusive
@@ -67,48 +71,48 @@ class Unit(object):
 
 
 class LandUnit(Unit):
-    def __init__(self,attack=0,defense=0,cost=0,move=0):
-        super().__init__(attack,defense,cost,move)
+    def __init__(self,attack=0,defense=0,cost=0,move=0,power=None):
+        super().__init__(attack,defense,cost,move,0,power)
 
     def unit_type(self):
         return "land"
         
     def __str__(self):
-        return "LandUnit"
+        return "landunit"
     
     def __repr__(self):
-        return "LandUnit"
+        return "landunit"
    
      
 class Infantry(LandUnit):
-    def __init__(self,attack=1,defense=2,cost=3,move=1):
-        super().__init__(attack,defense,cost,move)
+    def __init__(self,attack=1,defense=2,cost=3,move=1,power=None):
+        super().__init__(attack,defense,cost,move,0,power)
 
     def __str__(self):
-        return "Infantry"
+        return "infantry"
     
     def __repr__(self):
-        return "Infantry"
+        return "infantry"
 
 
 class Tank(LandUnit):
-    def __init__(self,attack=3,defense=2,cost=5,move=2):
-        super().__init__(attack,defense,cost,move)
+    def __init__(self,attack=3,defense=2,cost=5,move=2,power=None):
+        super().__init__(attack,defense,cost,move,0,power)
 
     def __str__(self):
-        return "Tank"
+        return "tank"
     
     def __repr__(self):
-        return "Tank"
+        return "tank"
 
 
 class AntiAircraft(LandUnit):
-    def __init__(self, attack=0,defense=1,cost=5,move=1):
-        super().__init__(attack, defense, cost, move)
+    def __init__(self, attack=0,defense=1,cost=5,move=1,power=None):
+        super().__init__(attack,defense,cost,move,0,power)
         
     def aagun_hits(self, air_units):
         """
-        Takes number of attacking units as a parameter
+        Takes number of attacking air units as a parameter
         Returns number of hits on air units
         """
         total_hits = 0
@@ -118,41 +122,41 @@ class AntiAircraft(LandUnit):
         return total_hits
 
     def __str__(self):
-        return "Anti_Aircraft"
+        return "antiaircraft"
     
     def __repr__(self):
-        return "Anti_Aircraft"
+        return "antiaircraft"
 
-
+	
 
 class AirUnit(Unit):
-    def __init__(self,attack=0,defense=0,cost=0,move=0):
-        super().__init__(attack, defense, cost, move)
-
+    def __init__(self,attack=0,defense=0,cost=0,move=0,power=None):
+        super().__init__(attack,defense,cost,move,0,power)
+        
     def unit_type(self):
         return "air"
 
     def __str__(self):
-        return "AirUnit"
+        return "airunit"
     
     def __repr__(self):
-        return "AirUnit"
+        return "airunit"
     
 
 class Fighter(AirUnit):
-    def __init__(self,attack=3,defense=4,cost=12,move=4):
-        super().__init__(attack, defense, cost, move)
+    def __init__(self,attack=3,defense=4,cost=12,move=4,power=None):
+        super().__init__(attack,defense,cost,move,0,power)
 
     def __str__(self):
-        return "Fighter"
+        return "fighter"
     
     def __repr__(self):
-        return "Fighter"
+        return "fighter"
         
         
 class Bomber(AirUnit):
-    def __init__(self,attack=4,defense=1,cost=15,move=6):
-        super().__init__(attack, defense, cost, move)
+    def __init__(self,attack=4,defense=1,cost=15,move=6,power=None):
+        super().__init__(attack,defense,cost,move,0,power)
         
     def bombing_raid(self, shot_down):
         """
@@ -165,43 +169,43 @@ class Bomber(AirUnit):
             return super.roll_die()
 
     def __str__(self):
-        return "Bomber"
+        return "bomber"
     
     def __repr__(self):
-        return "Bomber"
+        return "bomber"
         
         
         
 class SeaUnit(Unit):
-    def __init__(self,attack=0,defense=0,cost=0,move=2):
-        super().__init__(attack, defense, cost, move)
+    def __init__(self,attack=0,defense=0,cost=0,move=0,power=None):
+        super().__init__(attack,defense,cost,move,0,power)
 
     def unit_type(self):
         return "sea"
 
     def __str__(self):
-        return "SeaUnit"
+        return "seaunit"
     
     def __repr__(self):
-        return "SeaUnit"
+        return "seaunit"
     
 
-class Battleship(SeaUnit):
-    def __init__(self,attack=4,defense=4,cost=24):
-        super().__init__(attack, defense, cost)
+class Battleship(SeaUnit):#run some test cases on how this constructor works
+    def __init__(self,attack=4,defense=4,cost=24,move=2,power=None):
+        super().__init__(attack,defense,cost,move,0,power)
         
         #Do we want a bombard() method here, or will we add that in our combat function?
 
     def __str__(self):
-        return "Battleship"
+        return "battleship"
     
     def __repr__(self):
-        return "Battleship"
+        return "battleship"
     
 
 class Carrier(SeaUnit):
-    def __init__(self,attack=1,defense=3,cost=18,cargo=[]):
-        super().__init__(attack, defense, cost)
+    def __init__(self,attack=1,defense=3,cost=18,move=2,cargo=[],power=None):
+        super().__init__(attack,defense,cost,move,0,power)
         self.__cargo = cargo
 
     def add_cargo(self,new_cargo):
@@ -230,28 +234,28 @@ class Carrier(SeaUnit):
         return self.__cargo
 
     def __str__(self):
-        return "Carrier"
+        return "carrier"
     
     def __repr__(self):
-        return "Carrier"
+        return "carrier"
 
 
 class Submarine(SeaUnit):
-    def __init__(self,attack=2,defense=2,cost=8):
-        super().__init__(attack, defense, cost)
+    def __init__(self,attack=2,defense=2,cost=8,move=2,power=None):
+        super().__init__(attack,defense,cost,move,0,power)
     
     #do we want a first strike method, or does that go in the combat function?
 
     def __str__(self):
-        return "Submarine"
+        return "submarine"
     
     def __repr__(self):
-        return "Submarine"
+        return "submarine"
     
 
 class Transport(SeaUnit):
-    def __init__(self,attack=0,defense=1,cost=8,cargo=[]):
-        super().__init__(attack, defense, cost)
+    def __init__(self,attack=0,defense=1,cost=8,move=2,cargo=[],power=None):
+        super().__init__(attack,defense,cost,move,0,power)
         self.__cargo = cargo
 
     def add_cargo(self,new_cargo):
@@ -283,12 +287,23 @@ class Transport(SeaUnit):
         return self.__cargo
 
     def __str__(self):
-        return "Transport"
+        return "transport"
     
     def __repr__(self):
-        return "Transport"
+        return "transport"
 
+
+class IndustrialComplex(Unit):
+	def __init__(self,attack=0,defense=0,cost=15,move=0,power=None):
+        super().__init__(attack,defense,cost,move,0,power)
+		
+	def __str__(self):
+        return "industrial complex"
     
+    def __repr__(self):
+        return "industrial complex"
+
+	
 def main():
     boat = Carrier()
     print(boat.add_cargo(Fighter()))
