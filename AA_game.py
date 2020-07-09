c2768f7bb6b5b4ce793c31293a72e1c926197845
ZorenMarkov <46544351+ZorenMarkov@users.noreply.github.com>
2020-07-09 14:37:30 -0400

    Add files via upload

---
+++ AA_game.py
@@ -0,0 +1,65 @@
+##############################################################################
+#  This is code for the general game to run
+#  We can use this as the skeleton to build the cohesive game
+#  
+#
+#
+##############################################################################
+
+#Imports
+import Powers
+import Territorries
+import Units
+import Setup
+
+def buy_units(power):
+    #Will return the bought units as a list to be used later in the place units
+    pass
+
+def declare_attacks(power):
+    '''
+    declare attacks can handle whether attacks are legal as well as do the
+    edge cases like strategic bombings, bltizes, amphibious assaults, etc. and 
+    then it will call the combat function as many times as it needs
+    '''
+    
+    #combat(power, defender, units, territory)
+    
+def noncombat_move(power):
+    
+    pass
+
+def place_units(power, units):
+    #Uses the list created in the buy_units function
+    pass
+
+def collect_IPCs(power):
+    pass
+
+def take_turn(power):
+    buy_units(power)
+    declare_attacks(power)
+    noncombat_move(power)
+    place_units(power)
+    collect_IPCs(power)
+
+def check_win():
+    #Returns a boolean to check if a side has won
+    #True if so, False if not
+    pass
+
+def main():
+    #We can have the setup function return the 5 powers as a list
+    powers = Setup.setup()
+    
+    win = False
+    while not win:
+        for power in powers:
+            take_turn(power)
+        if check_win():
+            break
+    
+    
+
+if __name__ == '__main__':
+    main()
