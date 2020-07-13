35cb7dfffbfdf8ddd1363b76e5845bf6cbeefa10
Nathartacus <67975196+NJStriebel@users.noreply.github.com>
2020-07-13 15:18:44 -0400

    Create Movement.py

---
+++ Movement.py
@@ -0,0 +1,51 @@
+import Territories
+import Units as un
+
+def find_path(start_terr, end_terr, this_path=[], shortest_path=[]):
+    """
+    Takes the start territory and end territory of the path you want
+    Uses weird recursion and has not been tested! Test thoroughly as soon as setup is done!
+    Returns the shortest path between them, probably most useful for its length
+    """
+    if len(this_path) > len(shortest_path) and len(shortest_path) != 0:
+        return shortest_path
+    else:
+        
+        this_path.append(end_terr)
+
+        if start_terr in end_terr.adj_territories():
+            this_path.append(start_terr)
+            if len(this_path) < len(shortest_path) or len(shortest_path) == 0:
+                return this_path
+            else:
+                return shortest_path
+
+        valid_paths = []
+        for terr in end_terr.adj_territories():
+            #This if statement prevents backtracking
+            if not terr in this_path and (len(this_path) < len(shortest_path) or len(shortest_path) == 0):
+                #recursive call
+                next_path = find_path(start_terr, terr, this_path, shortest_path)
+                valid_paths.append()
+        
+        for path in valid_paths:
+            if len(path) < len(shortest_path) or len(shortest_path) == 0:
+                return path
+        
+        return shortest_path
+             
+
+def main():
+    """
+    Incomplete Test Case
+    """
+    terr1 = Territories.Territory("US", [], [], "terr1")
+    terr2 = Territories.Territory("US", [], [], "terr2")
+    terr3 = Territories.Territory("US", [], [], "terr3")
+    terr4 = Territories.Territory("US", [terr1, terr3], [], "terr4") 
+    
+
+    print find_path(terr1, terr4)
+
+if __name__ == "__main__":
+    main()
