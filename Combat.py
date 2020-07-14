dbd6b583a627b6243d5e1e1d43c1232e47729cd8
Brandon Geib <fluffboi9931@gmail.com>
2020-07-14 13:32:07 -0400

    Fully added the distinction between player and com, com code needs to be added in

--- Combat.py
+++ Combat.py
@@ -20,7 +20,7 @@ def get_num_hits(unit_list, is_attacker):
                 sub_hits += 1
     return total_hits,sub_hits
 
-def take_casualties(unit_list, num_dead, terrains=["sea","land","air"]):
+def take_casualties(unit_list, num_dead, terrains=["sea","land","air"], power):
     """
     Takes the list of units that casualties are being chosen from and the number of casualties
     Prompts for console input to get the type and number of units to remove
@@ -45,37 +45,64 @@ def take_casualties(unit_list, num_dead, terrains=["sea","land","air"]):
     if num_dead >= len(unit_list) and unit_list_has_only_valid_terrains:
         return unit_list,[]
     
-    
-    while num_dead > len(casualty_list) and len(unit_list) > 0:
-        print("You have to remove " + str(num_dead) + " units total.")
-        print("You have " + str(unit_list))
-	#Ask which unit type to remove and how many
-        unit_to_remove = input("What type of unit do you want to remove: ").lower()
-        how_many_to_remove = int(input("How many do you want to remove: "))
-        
-        num_of_removing_type = 0
-        remove_locations = []
-	#Find all of the units that we want to remove
-        for index,unit in enumerate(unit_list):
-            if str(unit) == str(unit_to_remove):
-                unit_to_remove = unit
-                num_of_removing_type += 1
-                remove_locations.append(index)
-        		
-	#Remove all of the ones that we found up until we have removed the number that we wanted to
-        if num_of_removing_type > 0 :
-            if unit_to_remove.unit_type() in terrains:
-                if how_many_to_remove <= (num_dead - len(casualty_list)) and how_many_to_remove > 0 and how_many_to_remove <= num_of_removing_type:
-                    num_removed = 0
-                    for index in remove_locations:
-                        if num_removed < how_many_to_remove:
-                            casualty_list.append(unit_list[index - num_removed])
-                            unit_list = unit_list[:index - num_removed] + unit_list[index - num_removed + 1:]
-                            num_removed += 1
+    if power.player():
+        while num_dead > len(casualty_list) and len(unit_list) > 0:
+            print("You have to remove " + str(num_dead) + " units total.")
+            print("You have " + str(unit_list))
+            #Ask which unit type to remove and how many
+            unit_to_remove = input("What type of unit do you want to remove: ").lower()
+            how_many_to_remove = int(input("How many do you want to remove: "))
+            
+            num_of_removing_type = 0
+            remove_locations = []
+
+            #Find all of the units that we want to remove
+            for index,unit in enumerate(unit_list):
+                if str(unit) == str(unit_to_remove):
+                    unit_to_remove = unit
+                    num_of_removing_type += 1
+                    remove_locations.append(index)
+                    
+            #Remove all of the ones that we found up until we have removed the number that we wanted to
+            if num_of_removing_type > 0 :
+                if unit_to_remove.unit_type() in terrains:
+                    if how_many_to_remove <= (num_dead - len(casualty_list)) and how_many_to_remove > 0 and how_many_to_remove <= num_of_removing_type:
+                        num_removed = 0
+                        for index in remove_locations:
+                            if num_removed < how_many_to_remove:
+                                casualty_list.append(unit_list[index - num_removed])
+                                unit_list = unit_list[:index - num_removed] + unit_list[index - num_removed + 1:]
+                                num_removed += 1
+                    else:
+                        print("That is not a valid number to remove")	
                 else:
-                    print("That is not a valid number to remove")	
-            else:
-                print("That unit type name is invalid. Remember that it must be from " + str(terrains))
+                    print("That unit type name is invalid. Remember that it must be from " + str(terrains))
+    else:
+        while num_dead > len(casualty_list) and len(unit_list) > 0:
+            #Ask which unit type to remove and how many
+            unit_to_remove = #COM CODE
+            how_many_to_remove = #COM CODE
+            
+            num_of_removing_type = 0
+            remove_locations = []
+
+            #Find all of the units that we want to remove
+            for index,unit in enumerate(unit_list):
+                if str(unit) == str(unit_to_remove):
+                    unit_to_remove = unit
+                    num_of_removing_type += 1
+                    remove_locations.append(index)
+                    
+            #Remove all of the ones that we found up until we have removed the number that we wanted to
+            if num_of_removing_type > 0 :
+                if unit_to_remove.unit_type() in terrains:
+                    if how_many_to_remove <= (num_dead - len(casualty_list)) and how_many_to_remove > 0 and how_many_to_remove <= num_of_removing_type:
+                        num_removed = 0
+                        for index in remove_locations:
+                            if num_removed < how_many_to_remove:
+                                casualty_list.append(unit_list[index - num_removed])
+                                unit_list = unit_list[:index - num_removed] + unit_list[index - num_removed + 1:]
+                                num_removed += 1
         
     return casualty_list, unit_list
 
@@ -96,7 +123,7 @@ def do_combat(attacker_units, defender_units, attacker_power, defender_power,bat
     returns the list of units remaining in the territory, the power that won
     """
     #Handle amphibious battleship hits 
-    temp_list, attackr_units = take_casualties(attacker_units, battleship_hits)
+    temp_list, attacker_units = take_casualties(attacker_units, battleship_hits, attacker_power)
     
     aagun_exists = False
     
@@ -110,7 +137,7 @@ def do_combat(attacker_units, defender_units, attacker_power, defender_power,bat
                     num_air_attackers += 1
             num_air_hits = def_unit.aagun_hits(num_air_attackers)
                 	
-            temp_list,attacker_units = take_casualties(attacker_units, num_air_hits,terrains=["air"])
+            temp_list,attacker_units = take_casualties(attacker_units, num_air_hits,terrains=["air"], attacker_power)
             
             defender_units = defender_units[:index] + defender_units[index+1:]
     
@@ -123,14 +150,14 @@ def do_combat(attacker_units, defender_units, attacker_power, defender_power,bat
         atk_hits, atk_sub_hits = get_num_hits(attacker_units, True)
         print("Attacker hits: " + str(atk_hits + atk_sub_hits))
         
-        temp_list, defender_units = take_casualties(defender_units, atk_sub_hits, terrains=["sea"])
-        casualty_zone, defender_units = take_casualties(defender_units, atk_hits)
+        temp_list, defender_units = take_casualties(defender_units, atk_sub_hits, terrains=["sea"], defender_power)
+        casualty_zone, defender_units = take_casualties(defender_units, atk_hits, defender_power)
         
         def_hits, def_sub_hits = get_num_hits(defender_units + casualty_zone, False)
         print("Defender hits: " + str(def_hits + def_sub_hits))		
         
-        temp_list, attacker_units = take_casualties(attacker_units, def_hits)
-        temp_list, attacker_units = take_casualties(attacker_units, def_sub_hits, terrains=["sea"])		
+        temp_list, attacker_units = take_casualties(attacker_units, def_hits, attacker_power)
+        temp_list, attacker_units = take_casualties(attacker_units, def_sub_hits, terrains=["sea"], attacker_power)		
         
         temp_list = []
         casualty_zone = []
